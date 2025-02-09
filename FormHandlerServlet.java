cd9ec3975f11958e6898efcde8d7cef634b1ea5b
vineelateki <vineelateki666@gmail.com>
2025-02-09 15:21:36 +0530

    Add files via upload

---
+++ FormHandlerServlet.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/submitForm")
+public class FormHandlerServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String message = request.getParameter("message");
+
+        out.println("<html>");
+        out.println("<head><title>Form Submission</title></head>");
+        out.println("<body>");
+        out.println("<h2>Thank you for your submission!</h2>");
+        out.println("<p><strong>Name:</strong> " + name + "</p>");
+        out.println("<p><strong>Email:</strong> " + email + "</p>");
+        out.println("<p><strong>Message:</strong> " + message + "</p>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
+
+
